cecb67d2d0f87ca7d80cc82a8b15481e91dceef3
Kelly Letícia <122893902+kellyleticia@users.noreply.github.com>
2024-11-08 10:49:29 -0300

    trying

--- Noir/ContentView.swift
+++ Noir/ContentView.swift
@@ -9,15 +9,54 @@ import SwiftUI
 import RealityKit
 
 struct ContentView: View {
-
+    @State var animationResources: [AnimationResource] = []
+    @State var model1 = try! Entity.loadModel(named: "robofeio.usdz")
+    
     var body: some View {
-        RealityView { content in
-            let model = await try! ModelEntity(named: "fenix")
-            model.transform.scale = SIMD3<Float>(0.001, 0.001, 0.001)
-            if let animation = model.availableAnimations.first {
-                model.playAnimation(animation.repeat())
+        //        VStack {
+        //            Model3D(named: "fenix.usdz") { model in
+        //                model
+        //                 .resizable()
+        //                 .aspectRatio(contentMode: .fit)
+        //            } placeholder: {
+        //                ProgressView()
+        //            }
+        //
+        //            Button("Change") {
+        //                model1.playAnimation(animationResources[0].repeat(), transitionDuration: 0.5, startsPaused: false)
+        //            }
+        //        }
+        VStack {
+            RealityView { content in
+                model1.transform.scale = SIMD3<Float>(0.001, 0.001, 0.001)
+                if let animation = model1.availableAnimations.first {
+                    model1.playAnimation(animation.repeat())
+                }
+                model1.playAnimation(animationResources[0].repeat(), transitionDuration: 0.5, startsPaused: false)
+                content.add(model1)
             }
-            content.add(model)
+            
+            Button("Change") {
+                model1.playAnimation(model1.availableAnimations[3].repeat(), transitionDuration: 0.5, startsPaused: false)
+            }
+        }
+        
+        .onAppear {
+            model1.availableAnimations.forEach({print($0.name)})
+            let definition = model1.availableAnimations[0].definition
+            let battle = AnimationView(
+                source: definition,
+                name: "default subtree animation",
+                bindTarget: nil,
+                blendLayer: 0,
+                repeatMode: .autoReverse,
+                fillMode: [])
+            
+            var clipResources: AnimationResource = try! AnimationResource.generate(with: battle)
+            model1.playAnimation(clipResources)
+            animationResources.append(clipResources)
+            
+            model1.playAnimation(animationResources[0].repeat(), transitionDuration: 0.5, startsPaused: false)
         }
     }
 }
@@ -26,3 +65,5 @@ struct ContentView: View {
     ContentView()
         .environment(AppModel())
 }
+
+
